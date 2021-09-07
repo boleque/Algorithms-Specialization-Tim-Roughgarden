c06e80dafc9abeba3dfd0a4d198d864b4785b378
Vladislav Kislitsyn <v_kislitsyn@wargaming.net>
2021-09-07 11:48:08 +0300

    Added counting inversions algorithm with merge sort

--- Divide and Conquer, Sorting and Searching, and Randomized Algorithms/week 2/counting_inversions_ii.py
+++ Divide and Conquer, Sorting and Searching, and Randomized Algorithms/week 2/counting_inversions_ii.py
@@ -8,26 +8,47 @@
 # right inversion i,j > n/2
 # split inversion if i <= n/2 < j
 
+from timeit import timeit
 
 def sort_and_count(arr):
     arrLen = len(arr)
-    if arrLen == 1:
-        return 0
+    if arrLen < 2:
+        return arr, 0
     middle = arrLen // 2
+    B, x = sort_and_count(arr[:middle])
+    C, y = sort_and_count(arr[middle:])
+    D, z = merge_and_count_split_inversion(B, C)
 
-    b, x = sort_and_count(arr[:middle])
-    c, y = sort_and_count(arr[middle:])
-    d, z = merge_and_count_split_inversion(b, c) 
+    return D, (x + y + z) 
 
-    return x + y + z
-
-def merge_and_count_split_inversion(b, c):
-    middle = len(arr) // 2
+def merge_and_count_split_inversion(B, C):
+    D = []
+    i = j = 0
     inv_count = 0
-    for i in range(middle):
-        for j in range(middle, len(arr)):
-        
-            if arr[i] > arr[j]:
-                inv_count += 1
 
-    return inv_count
+    while i < len(B) and j < len(C):
+        
+        if B[i] > C[j]:
+            D.append(C[j])
+            j += 1
+            inv_count += (len(B)-i) 
+        else:
+            D.append(B[i])
+            i += 1
+        
+    D.extend(B[i:])
+    D.extend(C[j:])
+
+    return D, inv_count
+
+ 
+if __name__ == '__main__':
+    input_array = None
+    with open('IntegerArray.txt') as f:
+        input_array = [int(x) for x in f.readlines()]
+    begin = timeit()
+    if input_array is not None:
+        arr, inv_count = sort_and_count(input_array)
+        print("Inversions number: ", inv_count) # 2407905288L
+    end = timeit()
+    print("Time: ", end - begin) # 0.0003689000000000114 
