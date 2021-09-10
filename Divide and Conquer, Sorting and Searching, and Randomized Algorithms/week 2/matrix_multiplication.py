78620e4f254da01938516315cdab36a64cd79781
Vladislav Kislitsyn <v_kislitsyn@wargaming.net>
2021-09-10 15:53:44 +0300

    Recursive matrix mult algorithm added

--- Divide and Conquer, Sorting and Searching, and Randomized Algorithms/week 2/matrix_multiplication.py
+++ Divide and Conquer, Sorting and Searching, and Randomized Algorithms/week 2/matrix_multiplication.py
@@ -1,51 +1,66 @@
-# X = A B 
-#     C D
-
-# Y = E F
-#     G H
-
-# XY = (AE + BG), (AF + AH)
-#      (CE + CG), (CF + DH)
-
 
 # Brute force matrix multiplication algorithm - O(n^3)
 def brute_force_matrix_multiplication(X, Y):
-    matrixSize = len(X)
-    res = [[0 for row in range(matrixSize)] for col in range(matrixSize)]
-    for i in range(matrixSize):
-        for j in range(matrixSize):
-            sum = 0
-            for k in range(matrixSize):
-                sum += X[i][k] * Y[k][j]
-            res[i][j] = sum
+    matrixLen = len(X)
+    res = [[0 for row in range(matrixLen)] for col in range(matrixLen)]
+    for i in range(matrixLen):
+        for j in range(matrixLen):
+            for k in range(matrixLen):
+                res[i][j] += X[i][k] * Y[k][j]
     return res
 
 
 # Recursive matrix multiplication algorithm - O(n^3)
-def recursive_matrix_multiplication(X, Y):
-    if len(X) == 1:
-        return X[0][0]*Y[0][0]
-    
-    A = None
-    B = None
-    C = None
-    D = None
+def recursive_matrix_multiplication(A, B):
+    return __recursive_matrix_multiplication(A, B, 0, 0, 0, 0, len(A))
 
-    E = None
-    F = None
-    G = None
-    H = None
-    
-    ae = recursive_matrix_multiplication()
-    bg = recursive_matrix_multiplication()
-    ce = recursive_matrix_multiplication()
-    cg = recursive_matrix_multiplication()
-    af = recursive_matrix_multiplication()
-    ah = recursive_matrix_multiplication()
-    cf = recursive_matrix_multiplication()
-    dh = recursive_matrix_multiplication()
+def __recursive_matrix_multiplication(A, B, rowA, colA, rowB, colB, size):    
+    C = [[0 for row in range(size)] for col in range(size)]
+    if size == 1:
+        C[0][0] = A[rowA][colA] * B[rowB][colB];
+    else:
+        size //= 2
+        # C11
+        add_matrix(
+            __recursive_matrix_multiplication(A, B, rowA, colA, rowB, colB, size), # A11 * B11
+            __recursive_matrix_multiplication(A, B, rowA, colA+size, rowB+size, colB, size), # A12 * B21
+            C,
+            0,
+            0
+        )
+        # C12
+        add_matrix(
+            __recursive_matrix_multiplication(A, B, rowA, colA, rowB, colB+size, size), # A11 * B12
+            __recursive_matrix_multiplication(A, B, rowA, colA+size, rowB+size, colB+size, size), # A12 * B22
+            C,
+            0,
+            size
+        )
+        # C21 
+        add_matrix(
+            __recursive_matrix_multiplication(A, B, rowA+size, colA, rowB, colB, size), # A21 * B11 
+            __recursive_matrix_multiplication(A, B, rowA+size, colA+size, rowB+size, colB, size), # A22 * B21
+            C,
+            size,
+            0
+        )
+        # C22
+        add_matrix(
+            __recursive_matrix_multiplication(A, B, rowA+size, colA, rowB, colB+size, size), # A21 * B12 
+            __recursive_matrix_multiplication(A, B, rowA+size, colA+size, rowB+size, colB+size, size), # A22 * B22
+            C,
+            size,
+            size
+        )
 
-    return
+    return C
+
+def add_matrix(A, B, C, rowC, colC):
+    lenA = len(A)
+    for i in range(lenA):
+        for j in range(lenA):
+            C[i+rowC][j+colC] = A[i][j] + B[i][j]
+
 
 # Strassen strassen's subcubic matrix multiplication algorithm - O(n^2)
 # P1 = A(F-H)
@@ -60,17 +75,23 @@ def recursive_matrix_multiplication(X, Y):
 #      (P3 + P),(P1 + P5 - P3 - P7)
 
 
-
 if __name__ == '__main__':
     
     X = [
-        [1,2,3], 
-        [4,5,6], 
-        [7,8,9]
+        [5,6,5,6],
+        [7,8,5,6],
+        [7,8,5,6],
+        [7,8,5,6],
     ]
     
     Y = [
-        [9,3,1], 
-        [4,8,3], 
-        [5,6,1]
+        [1,2,1,2],
+        [3,4,1,2],
+        [1,2,1,2],
+        [1,2,1,2],
     ]
+
+    res1 = recursive_matrix_multiplication(X, Y)
+    res2 = brute_force_matrix_multiplication(X, Y)
+    
+    print(">>> EQUAL={} rec={} brut={}".format(res1 == res2, res1, res2))
