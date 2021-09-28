4c5feb3daa0de969bb7828d0deb72e175935be0e
Vladislav Kislitsyn <v_kislitsyn@wargaming.net>
2021-09-28 17:48:06 +0300

    divide and conquer closest pair

--- Divide and Conquer, Sorting and Searching, and Randomized Algorithms/week 2/closest_pair_i.py
+++ Divide and Conquer, Sorting and Searching, and Randomized Algorithms/week 2/closest_pair_i.py
@@ -16,7 +16,7 @@ def closest_pair_1d(P):
             res = arrSorted[i+1], arrSorted[i]
     return res
 
-class Point(object)
+class Point(object):
     
     def __init__(self, x, y):
         self._x = x
@@ -24,9 +24,12 @@ class Point(object)
 
     def distance(self, other):
         return math.sqrt(pow(self._x - other._x) + pow(self._y - other._y))
+    
+    def __repr__(self):
+        return "Point x={} y={}".format(self._x, self._y)
 
 def closest_pair_2D_brute_force(P):
-    res = None
+    best_pair = None
     minDistance = sys.maxint
     pLen = len(P)
     for i in range(pLen):
@@ -34,41 +37,58 @@ def closest_pair_2D_brute_force(P):
             distance = P[i].distance(P[j])
             if minDistance > distance:
                 minDistance = distance
-                res = P[i], P[j]
-    return res
+                best_pair = P[i], P[j]
+    return best_pair
 
 def closest_pair_2D_divide_and_conquer(Px, Py):
+    pLen = len(Px)
+    if pLen <= 3:
+        return closest_pair_2D_brute_force(Px)
     
-    if True:
-        return closest_pair_2D_brute_force()
+    #Q - left half of P
+    #R - right half of P
+    middle = pLen // 2
+    Qx, Qy = Px[:middle], Py[:middle]
+    Rx, Ry = Px[middle:], Py[middle:]
 
-    p1, q1 = closest_pair_2D_divide_and_conquer()
-    p2, q2 = closest_pair_2D_divide_and_conquer()
+    p1, q1 = closest_pair_2D_divide_and_conquer(Qx, Qy)
+    p2, q2 = closest_pair_2D_divide_and_conquer(Rx, Ry)
 
     delta = min(p1.distance(q1), p2.distance(q2))
 
-    p3, q3 = closest_pair_2D_divide_and_conquer()
+    p3, q3 = split_closest_pair(Px, Py, delta)
 
-    return (p1, q1), (p2, q2), (p3, q3)
+    return best of (p1, q1), (p2, q2), (p3, q3)
+
+def split_closest_pair(Px, Py, delta):
+    pLen = len(Px)
+    # x - max coord in left of P
+    x = Px[:pLen // 2][-1]._x
+    # let Sy be the points of P with x within the range
+    Sy = [p for p in Py if (x - delta) <= p._x =< (x + delta)]
+
+    best = delta
+    best_pair = None
+    sLen = len(Sy)
     
-def split_closest_pair():
-    pass
+    points_to_check_i, points_to_check_j = sLen, sLen if sLen <= 7 else (Sy - 7), 7
+    for i in range(points_to_check_i):
+        for j in (i + 1, points_to_check_j):
+            distance = Sy[i].distanceTo(Sy[j])
+            if distance < best:
+                best = distance
+                best_pair = Sy[i], Sy[j]
+    return best_pair
 
 if __name__ == '__main__':
     arr = [1, 6, 5, 10, 25, 13, 11, 19]
     P = [
-        Point(),
-        Point(),
-        Point(),
-        Point(),
-        Point(),
-        Point(),
-        Point(),
-        Point(),
-        Point(),
+        Point(7,4),
+        Point(2,2),
+        Point(4,5),
+        Point(2,6),     
+        Point(1,1),
     ]
-    
-    Px = sorted(P)
-    Py = sorted(P)
-    res = closestPair2D(Px, Py)
-    print('>>> result ', res)
+
+    Px = sorted(P, key=lambda p: p._x)
+    Py = sorted(P, key=lambda p: p._y)
