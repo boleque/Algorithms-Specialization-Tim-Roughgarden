7cef5baf52a71a6fc62a0a8e43d967aafb825e16
Vladislav Kislitsyn <v_kislitsyn@wargaming.net>
2021-10-08 10:24:22 +0300

    assignment #3 done

--- Divide and Conquer, Sorting and Searching, and Randomized Algorithms/week 3/quicksort.py
+++ Divide and Conquer, Sorting and Searching, and Randomized Algorithms/week 3/quicksort.py
@@ -1,46 +1,53 @@
-def quickSort(arr, leftBoundary, rightBoundary):
-    if leftBoundary < rightBoundary:
-        partitionBoundary = __partition(arr, leftBoundary, rightBoundary)
-        quickSort(arr, leftBoundary, partitionBoundary-1)
-        quickSort(arr, partitionBoundary+1, rightBoundary)
 
-def __partition(arr, leftBoundary, rightBoundary):
-    i = leftBoundary - 1
-    pivot = arr[rightBoundary]
-    for j in range(leftBoundary, rightBoundary):
+def quick_sort(arr, left_idx, right_idx, mode):
+    if left_idx < right_idx:
+        partition_idx = __partition(arr, left_idx, right_idx, mode)
+        quick_sort(arr, left_idx, partition_idx - 1, mode)
+        quick_sort(arr, partition_idx + 1, right_idx, mode)
+
+def __partition(arr, left_idx, right_idx, mode):
+    global comparisons
+
+    if mode == 'first':
+        pivot_idx = left_idx
+    elif mode == 'last':
+        pivot_idx = right_idx
+    elif mode == 'mean':
+        pivot_idx = median_of_three(arr, left_idx, right_idx)
+
+    # important: swap elements to ensure pivot on the left most position
+    arr[pivot_idx], arr[left_idx] = arr[left_idx], arr[pivot_idx]
+    i = left_idx + 1
+    pivot = arr[left_idx]
+    for j in range(left_idx + 1, right_idx + 1):
         if arr[j] <= pivot:
+            arr[i], arr[j] = arr[j], arr[i]
             i += 1
-            arr[i], arr[j] = arr[j], arr[i]           
-    arr[i+1], arr[rightBoundary] = arr[rightBoundary], arr[i+1]
-    return i + 1
+    arr[i - 1], arr[left_idx] = arr[left_idx], arr[i - 1]
+    # important: number of comparisons are equal to distance between right and left boundaries
+    comparisons += right_idx - left_idx
+    return i - 1
 
-#if __name__ == '__main__':
-#    arr1 = [8,2,3,5,1,4,7,6]
-#    arr2 = [3,2,8,5,1,4,7,6]
-#
-#    quickSort(arr1, 0, len(arr1)-1)  
-#    quickSort(arr2, 0, len(arr2)-1)  
-#    print('>> Res: ', arr1, arr2)
-   
-#class PIVOT_MODE:
-#	EXTREME_LEFT = 0
-#	EXTREME_RIGHT = 1
-#	MEAN_OF_THREE = 2
-    
-#def get_mean_of_three(a, left, right):
-#    mid = (left + right) // 2
-#    li = [(a[left], left), (a[mid], mid), (a[right], right)]
-#    li.sort()
-#    return li[1][1]
+def median_of_three(arr, left_idx, right_idx):
+    mid_idx = left_idx + (right_idx - left_idx) // 2
+    li = [(arr[left_idx], left_idx), (arr[mid_idx], mid_idx), (arr[right_idx], right_idx)]
+    li.sort(key=lambda x: x[0])
+    return li[1][1]
 
-#if __name__ == "__main__":
-#	
-#	with open("QuckSortInput.txt") as f:
-#		input = [int(x) for x in f.readlines()]
-#	#print("input ", len(input))
-#	comparisons = 0
-#	quick_sort(input, 0, len(input) - 1)
-#	res = "RESULT: {}".format(comparisons)
-#	print(res)
-#
-## left 162085 right 164123 mean 138382
+if __name__ == "__main__":
+    array = []
+    with open("QuickSort.txt") as f:
+        array = [int(x) for x in f.readlines()]
+    arrayLen = len(array)
+
+    comparisons = 0    
+    quick_sort(list(array), 0, arrayLen - 1, 'first')
+    print("\n>> Left most pivot; Number of comparisons: {}".format(comparisons)) # 162085
+
+    comparisons = 0    
+    quick_sort(list(array), 0, arrayLen - 1, 'last')
+    print("\n>> Right most pivot; Number of comparisons: {}".format(comparisons)) # 164123
+
+    comparisons = 0    
+    quick_sort(list(array), 0, arrayLen - 1, 'mean')
+    print("\n>> Mean pivot; Number of comparisons: {}".format(comparisons)) # 138382
