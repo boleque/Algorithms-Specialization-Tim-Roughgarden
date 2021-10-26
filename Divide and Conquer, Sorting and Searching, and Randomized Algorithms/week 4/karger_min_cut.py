44a74690a6ea2c7571275dfd920358a2af1bcc3f
Vladislav Kislitsyn <v_kislitsyn@wargaming.net>
2021-10-26 11:37:06 +0300

    min cut problem done

--- Divide and Conquer, Sorting and Searching, and Randomized Algorithms/week 4/karger_min_cut.py
+++ Divide and Conquer, Sorting and Searching, and Randomized Algorithms/week 4/karger_min_cut.py
@@ -1,27 +1,26 @@
-
 import random
+import copy
+import sys
 
-def choose_random_edge(graph):
-    u = random.choice(list(graph.keys()))
-    v = random.choice(graph[u])
-    return u, v
+def merge(graph, u, v):
+    for child in graph[v]:  
+        graph[child] = [node if node != v else u for node in graph[child]]
+        graph[u].append(child)
+    graph[u] = [node for node in graph[u] if node != u]
+    del graph[v]
 
 def min_cut(graph):
-	v = 0
-	while len(graph) > 2:
-		v, u = choose_random_edge(graph)
-		contract(graph, v, u)
-	return len(graph[v])
+    graph = copy.deepcopy(graph)
+    verticies = graph.keys()
+    while len(verticies) > 2:
+        u = random.choice(verticies)
+        v = random.choice(graph[u])
+        verticies.remove(v) # todo remove with constant time
+        merge(graph, u, v)
+    else:
+        return len(graph[u])
 
-def contract(graph, v, u):
-	for node in graph[u]:
-		if node != v:
-			graph[v].append(node) # merge all u's children to v
-		graph[node].remove(u) 	  # remove merged vertex from all children
-		if node != v:
-			graph[node].append(v) # add vertex over removed vertex u
-	del graph[u]	
-
+    
 if __name__ == "__main__":
     graph = {}
     with open('kargerMinCut.txt') as f:
@@ -30,3 +29,27 @@ if __name__ == "__main__":
             if line:
                 verticies = [int(x) for x in line]
                 graph[verticies[0]] = verticies[1:]
+        
+        N10 = 10
+        min_cuts10 = []
+        for _ in range(N10):
+            min_cuts10.append(min_cut(graph))
+        res10 = min(min_cuts10)
+        print('>> For N={} min cut={}\n'.format(N10, res10))
+        
+        N100 = 100
+        min_cuts100 = []
+        for _ in range(N100):
+            min_cuts100.append(min_cut(graph))
+        res100 = min(min_cuts100)
+        print('>> For N={} min cut={}\n'.format(N100, res100))
+        
+        N1000 = 1000
+        min_cuts1000 = []
+        for _ in range(N100):
+            min_cuts1000.append(min_cut(graph))
+        res1000 = min(min_cuts1000)
+        print('>> For N={} min cut={}\n'.format(N1000, res1000))
+
+        print('>> min cut ', min(res1, res10, res100, res1000)) # 17
+        
