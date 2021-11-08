d4aa7887fe0d909bcf1d2b7e764ff95d5c56b4b1
boleque <simple108@mail.ru>
2021-11-08 23:18:52 +0300

    strong componets assignment

--- Graph Search, Shortest Paths, and Data Structures/week 1/DFS_strong_components.py
+++ Graph Search, Shortest Paths, and Data Structures/week 1/DFS_strong_components.py
@@ -1,63 +1,85 @@
+
 import sys
+import resource
+from collections import defaultdict, Counter
 
-t = 0 # number of nodes processed so far, for finishing times in 1st pass
-s = None # most recent vertex from wich dfs was initiated, for leaders in 2nd pass
-leaders = {} # len of graph
-finishing_time = {} # len of graph
 
-def dfs(graph, vertex, visited, reverse=False):
-    global leaders, finishing_time
-    global t, s
+resource.setrlimit(resource.RLIMIT_STACK, (2**30, -1))
+sys.setrecursionlimit(10**6)
+
+
+def dfs_topological_order(graph, vertex, visited):
+    global node_time, finishing_times
 
     visited.add(vertex)
-    leaders[vertex] = s
-    if reverse:
-        pass # todo substitute node with it neighbor to reverse direction        
-    for neighbor in graph[vertex]:
+    for neighbor in graph.get(vertex, []):
         if neighbor not in visited:
-            dfs(graph, neighbor, visited)
-    t += 1
-    finishing_time[vertex] = t
+            dfs_topological_order(graph, neighbor, visited)
+    finishing_times[node_time] = vertex
+    node_time -= 1
 
-def dfs_loop(graph, nodes):
-    global s
+def dfs_scc(graph, vertex, visited):
+    global leaders, current_leader
 
+    leaders[current_leader].append(vertex)
+    visited.add(vertex)
+    for neighbor in graph.get(vertex, []):
+        if neighbor not in visited:
+            dfs_scc(graph, neighbor, visited)
+
+def dfs_loop(graph, verticies, loop_fun):
+    global current_leader, leaders
+
+    current_leader = None
     visited = set()
-    # start interation from max node
-    nodes.sort(reverse=True)
-    for node in nodes:
-        if node not in visited:
-            s = node
-            dfs(graph, node, visited)
+    leaders = defaultdict(list)
+    for vertex in verticies:
+        if vertex not in visited:
+            current_leader = vertex
+            loop_fun(graph, vertex, visited)
 
-def strong_components(graph):
-    nodes = graph.keys()
-    dfs_loop(graph, nodes, reverse=True)
+def strong_components(graph, graph_reversed, nodes_num):
+    global node_time, finishing_times
+
+    node_time = nodes_num - 1
+    finishing_times = [0] * nodes_num
+
+    dfs_loop(graph_reversed, range(1, nodes_num), dfs_topological_order)
+    dfs_loop(graph, finishing_times, dfs_scc)
+
+def most_common(leader, x):
+    results = [len(v) for k, v in leader.items()]
+
+    return sorted(results, reverse=True)[: x]
+
+def load_graph(filename):
+    graph = {}
+    graph_reversed = {}
+    unique_nodes = set()
+    with open(filename, 'r') as source:
+        for line in source:
+            values = line.split()
+            if values:
+                vertex = int(values[0])
+                neighbor = int(values[1])
+
+                neighbors = graph.setdefault(vertex, [])                
+                neighbors.append(neighbor)
+
+                neighbors_reversed = graph_reversed.setdefault(neighbor, [])
+                neighbors_reversed.append(vertex)
+
+                unique_nodes.add(vertex)
+                unique_nodes.add(neighbor)
+    return graph, graph_reversed, len(unique_nodes)
+
+def five_largest_SCCs():
+    global leaders
+
+    graph, graph_reversed, nodes = load_graph("SCC.txt")
+    SCCs = strong_components(graph, graph_reversed, nodes)
+    return sorted([len(v) for k, v in leaders.items()], reverse=True)[:5]
 
 if __name__ == '__main__':
-    # for testing
-    graph = {
-        1: [7],
-        2: [5],
-        3: [9],
-        4: [1],
-        5: [8],
-        6: [3, 8],
-        7: [4, 9],
-        8: [2],
-        9: [6],
-    
-    }
-
-    #with open('SCC.txt', 'r') as source:
-    #    for line in source:
-    #        values = line.split()
-    #        if len(values) >= 1:
-    #            vertex = int(values[0])
-    #            neighbors = graph.setdefault(vertex, [])
-    #            neighbors.append(int(values[1]))
-
-    SCCs = strong_components(graph)
-    if SCCs:
-        SCCs.sort()
-        print('5 first maximum strong components: ', SCCs[:5])
+    print('result: ', five_largest_SCCs())
+    # 434821, 968, 459, 313, 211
