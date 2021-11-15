7b5e079896754f935db2e1887331b3cd34b826ba
Vladislav Kislitsyn <v_kislitsyn@wargaming.net>
2021-11-15 09:49:48 +0300

    dijkstra implementation

---
+++ Graph Search, Shortest Paths, and Data Structures/week 2/shortest_path.py
@@ -0,0 +1,54 @@
+import heapq
+from collections import defaultdict, namedtuple
+
+
+def load_graph():
+	with open("dijkstraData.txt") as f:
+		content = f.readlines()
+		content = [x.strip('\n') for x in content]
+		graph = defaultdict(list)
+		for line in content:
+			line_entries = line.split()
+			parent_vtx = int(line_entries[0])
+			for target, dist in (l.split(',') for l in line_entries[1:]):
+				graph[parent_vtx].append((int(target), int(dist)))
+		return graph
+
+def dijkstra(graph, start_vtx, target_vtx):
+    A = {} # computed shortest path distances
+    queue = [(0, start_vtx)]
+    visited = set()
+    while queue:
+        current_vtx_dist, current_vtx = heapq.heappop(queue)
+        if current_vtx == target_vtx: 
+            break
+        visited.add(current_vtx)
+        for neighbor, dist in graph[current_vtx]:
+            if neighbor not in visited:
+                new_dist = current_vtx_dist + dist
+                if new_dist < A.get(neighbor, float('inf')):
+                    A[neighbor] = new_dist
+                    heapq.heappush(queue, (new_dist, neighbor))
+    return A
+
+def shortest_path(graph, start_vtx, target_vtx):
+    A = dijkstra(graph, start_vtx, target_vtx)
+    return A[target_vtx]
+    
+if __name__ == '__main__':
+    graph = load_graph()
+    start_vertex = 1
+    target_verticies = (7, 37, 59, 82, 99, 115, 133, 165, 188, 197)
+    for target_vertex in target_verticies:
+        print('shortest path={} from={} to={}'.format(shortest_path(graph, start_vertex, target_vertex), start_vertex, target_vertex))
+
+    # shortest path=2599 from=1 to=7
+    # shortest path=2610 from=1 to=37
+    # shortest path=2947 from=1 to=59
+    # shortest path=2052 from=1 to=82
+    # shortest path=2367 from=1 to=99
+    # shortest path=2399 from=1 to=115
+    # shortest path=2029 from=1 to=133
+    # shortest path=2442 from=1 to=165
+    # shortest path=2505 from=1 to=188
+    # shortest path=3068 from=1 to=197
