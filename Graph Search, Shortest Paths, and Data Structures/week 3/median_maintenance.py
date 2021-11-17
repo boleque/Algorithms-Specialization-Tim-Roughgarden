e8c377164df3367e0991a8326ec24d4b7c1fa3d1
Vladislav Kislitsyn <v_kislitsyn@wargaming.net>
2021-11-17 11:33:51 +0300

    median maintenance task

---
+++ Graph Search, Shortest Paths, and Data Structures/week 3/median_maintenance.py
@@ -0,0 +1,30 @@
+import heapq
+
+
+def get_median():
+	min_heap = []
+    max_heap = []
+
+def get_data():
+	with open("median.txt") as f:
+		return (int(x.strip('\n')) for x in f.readlines())
+
+def rebalance(min_heap, max_heap):
+    pass
+
+def add_number(number, min_heap, max_heap):
+    pass
+
+def get_medians_sum(array):
+	medians_sum = 0
+	min_heap, max_heap = [], []
+	for num in array:
+		add_number(num, min_heap, max_heap)
+		rebalance(min_heap, max_heap)
+		medians_sum += get_median(min_heap, max_heap)
+	return medians_sum
+
+if __name__ == "__main__":
+	input_array = get_data()
+	median_sum = get_medians_sum(input_array)
+	print(median_sum % 10000)
