7246d093362bbed86059399eceaf5238dacafa89
boleque <simple108@mail.ru>
2021-11-28 01:51:06 +0300

    mst cost done

--- Greedy Algorithms, Minimum Spanning Trees, and Dynamic Programming/week 1/prim_mst_algorithm.py
+++ Greedy Algorithms, Minimum Spanning Trees, and Dynamic Programming/week 1/prim_mst_algorithm.py
@@ -11,60 +11,55 @@
 #For example, the third line of the file is "2 3 -8874", indicating that there is an edge connecting vertex #2 and vertex #3 that has cost -8874. 
 #
 #You should NOT assume that edge costs are positive, nor should you assume that they are distinct.
-#
 #Your task is to run Prim's minimum spanning tree algorithm on this graph.  You should report the overall cost of a minimum spanning tree --- an integer, which may or may not be negative --- in the box below. 
-#
 #IMPLEMENTATION NOTES: This graph is small enough that the straightforward O(mn) time implementation of Prim's algorithm should work fine. OPTIONAL: For those of you seeking an additional challenge, try implementing a heap-based version. The simpler approach, which should already give you a healthy speed-up, is to maintain relevant edges in a heap (with keys = edge costs).  The superior approach stores the unprocessed vertices in the heap, as described in lecture.  Note this requires a heap that supports deletions, and you'll probably need to maintain some kind of mapping between vertices and their positions in the heap.
-import random
+
+from collections import namedtuple, defaultdict
+
+Vertex = namedtuple('Vertex', ['cost', 'id', 'parent_id'])
 
 
 def get_data():
-	with open("edges.txt") as f:
-		content = f.readlines()
-		content = [x.strip('\n') for x in content]
-		graph = defaultdict(list)
-		for line in content:
-			line_entries = line.split()
-			parent_vtx = int(line_entries[0])
-			for target, dist in (l.split(',') for l in line_entries[1:]):
-				graph[parent_vtx].append((int(target), int(dist)))
-		return graph
+    with open("edges.txt") as f:
+        lines = iter(f.readlines())
+        number_of_nodes, number_of_edges = [int(value) for value in next(lines).split()]
+        graph = defaultdict(list)
+        for line in lines:
+            vertex, neighbor, cost = [int(v) for v in line.split()]
+            graph[vertex].append((neighbor, cost))
+            graph[neighbor].append((vertex, cost))
+        return number_of_nodes, number_of_edges, graph
 
+def get_next_vertex(min_dist_table):
+    min_vertex = Vertex(float('inf'), None, None)
+    for vertext_obj in min_dist_table:
+        if min_vertex.cost > vertext_obj.cost:
+            min_vertex = vertext_obj
 
-def prim_mst_classic_impl(graph):
-    paths = {} # computed shortest path distances from start vertex
-    queue = [(0, start_vtx)]
+    min_dist_table[min_vertex.id - 1] = Vertex(float('inf'), min_vertex.id, min_vertex.parent_id)
+    return min_vertex
+
+def prim_mst_classic_impl(graph, number_of_nodes, start_vertex):
+    mst = []
     visited = set()
-    
-    all_verticies = graph.keys()
-    start_vtx = random.choice(all_verticies)
+    min_dist_table = [
+        Vertex(float('inf'), vertex, None) for vertex in range(1, number_of_nodes + 1)
+    ]
 
-
-def prim_mst_heap_impl(graph):
-    all_verticies = graph.keys()
-    start_vtx = random.choice(all_verticies)
-    while queue:
-        current_vtx_dist, current_vtx = heapq.heappop(queue)
-        if current_vtx == target_vtx: 
-            break
-        visited.add(current_vtx)
-        for neighbor, dist in graph[current_vtx]:
+    min_dist_table[start_vertex - 1] = Vertex(0, start_vertex, None)
+    while len(visited) != number_of_nodes:
+        next_vertex = get_next_vertex(min_dist_table)
+        mst.append(next_vertex)
+        for neighbor, cost in graph[next_vertex.id]:
             if neighbor not in visited:
-                new_dist = current_vtx_dist + dist
-                if new_dist < paths.get(neighbor, float('inf')):
-                    paths[neighbor] = new_dist
-                    heapq.heappush(queue, (new_dist, neighbor))
-    return paths.get(target_vtx, float('inf'))
+                if min_dist_table[neighbor - 1].cost > cost:
+                    min_dist_table[neighbor - 1] = Vertex(cost, neighbor, next_vertex.id)
+        visited.add(next_vertex.id)
+    return mst
 
-if __name__ == '__main__':
-    test_graph = {
-        'a': [('b', 4), ('h', 8)],
-        'b': [('a', 4), ('c', 8), ('h', 11)],
-        'c': [('b', 8), ('d', 7), ('f', 4), ('i', 2)],
-        'd': [('c', 7), ('e', 9), ('f', 14)],
-        'e': [('d', 9), ('f', 10)],
-        'f': [('c', 4), ('g',2), ('d',14), ('e',10)],
-        'h': [('b', 11),('a', 8), ('g', 1), ('i', 7)],
-        'g': [('i', 6),('h', 1),('f', 2)],
-        'i': [('c', 2),('h', 7),('g', 6)],
-    }
+def mst_total_cost(number_of_nodes, graph):
+    start_vertex = 1
+    mst = prim_mst_classic_impl(graph, number_of_nodes, start_vertex)
+    return sum(node.cost for node in mst)
+
+# -3612829
