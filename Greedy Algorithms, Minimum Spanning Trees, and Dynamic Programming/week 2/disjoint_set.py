bc2656efd3efae9e2aaf71368d8307d6e2694dd2
Vladislav Kislitsyn <v_kislitsyn@wargaming.net>
2021-12-02 17:45:33 +0300

    Update disjoint_set.py

--- Greedy Algorithms, Minimum Spanning Trees, and Dynamic Programming/week 2/disjoint_set.py
+++ Greedy Algorithms, Minimum Spanning Trees, and Dynamic Programming/week 2/disjoint_set.py
@@ -1,5 +1,11 @@
-def find(x):
-    pass # return the name of the group in which obj belongs
 
-def union(group1, group2): 
-    pass # fuse groups
+class DisjointSet(object):
+    
+    def __init__(self):
+        pass
+
+    def find(x):
+        pass # return the name of the group in which obj belongs
+    
+    def union(group1, group2): 
+        pass # fuse groups
