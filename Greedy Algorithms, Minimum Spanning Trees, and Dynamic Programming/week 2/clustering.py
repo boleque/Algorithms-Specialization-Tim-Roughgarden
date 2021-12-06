261ceaf6650bd9fab081dda5ad808ce9d5d4515b
Vladislav Kislitsyn <v_kislitsyn@wargaming.net>
2021-12-06 10:32:25 +0300

    Update clustering.py

--- Greedy Algorithms, Minimum Spanning Trees, and Dynamic Programming/week 2/clustering.py
+++ Greedy Algorithms, Minimum Spanning Trees, and Dynamic Programming/week 2/clustering.py
@@ -18,7 +18,8 @@ def maximum_spacing(edges_sorted, clusters):
         node1_cluster = clusters.get_node_by_id(node1)
         node2_cluster = clusters.get_node_by_id(node2)
         if clusters.find(node1_cluster) != clusters.find(node2_cluster):
-            max_spacing = max(max_spacing, cost)
+            max_spacing = cost
+            break
     return max_spacing
 
 def k_clustering(edges_sorted, k=4):
@@ -27,14 +28,13 @@ def k_clustering(edges_sorted, k=4):
     for node1, node2, _cost in edges_sorted:
         disjoint_set.make_set(node1)
         disjoint_set.make_set(node2)
-    
+
     edges_sorted_iter = iter(edges_sorted)
-    while disjoint_set.sets_counter >= k:
+    while disjoint_set.sets_counter > k:
         node1, node2, _cost = next(edges_sorted_iter)
         node1_cluster = disjoint_set.get_node_by_id(node1)
         node2_cluster = disjoint_set.get_node_by_id(node2)
         disjoint_set.union(node1_cluster, node2_cluster)
-
     return disjoint_set
 
 def get_data():
@@ -53,6 +53,3 @@ if __name__ == '__main__':
     number_of_nodes, edges_sorted = get_data()
     clusters = k_clustering(edges_sorted)
     max_spacing = maximum_spacing(edges_sorted, clusters)
-    print('>> max_spacing:', max_spacing)
-    
-# 9997
