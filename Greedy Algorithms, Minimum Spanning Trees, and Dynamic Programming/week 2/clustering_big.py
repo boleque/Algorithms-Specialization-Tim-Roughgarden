69f19ba437e40e76e3ea5076341e84469f28af72
Vladislav Kislitsyn <v_kislitsyn@wargaming.net>
2021-12-10 17:30:57 +0300

    clustering big data task

---
+++ Greedy Algorithms, Minimum Spanning Trees, and Dynamic Programming/week 2/clustering_big.py
@@ -0,0 +1,18 @@
+from disjoint_set import DisjointSet
+
+#[# of nodes] [# of bits for each node's label]
+#[first bit of node 1] ... [last bit of node 1]
+#[first bit of node 2] ... [last bit of node 2]
+
+def get_data():
+    with open("clustering_big.txt") as f:
+        nodes = {}
+        lines = iter(f.readlines())
+        bits_and_nodes_info_str = next(lines)
+        nodes_number, bits_number = [int(v) for v in bits_and_nodes_info_str.split()]      
+        for node, bit_distance in enumerate(lines, 1):
+            nodes[bit_distance] = node
+        return nodes_number, bits_number, nodes
+        
+if __name__ == '__main__':
+    get_data()
