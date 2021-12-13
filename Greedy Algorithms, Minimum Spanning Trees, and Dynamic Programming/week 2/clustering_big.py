9af5c2755b2cc81025e1230b5c91bcbd9135cff2
boleque <simple108@mail.ru>
2021-12-13 07:22:37 +0300

    clustering big update

--- Greedy Algorithms, Minimum Spanning Trees, and Dynamic Programming/week 2/clustering_big.py
+++ Greedy Algorithms, Minimum Spanning Trees, and Dynamic Programming/week 2/clustering_big.py
@@ -1,4 +1,11 @@
+import re
+import sys
+import time
 from disjoint_set import DisjointSet
+from collections import defaultdict
+from itertools import combinations
+
+sys.setrecursionlimit(10000)
 
 #[# of nodes] [# of bits for each node's label]
 #[first bit of node 1] ... [last bit of node 1]
@@ -8,44 +15,51 @@ from disjoint_set import DisjointSet
 #with spacing at least 3?  That is, how many clusters are needed to ensure that no pair 
 #of nodes with all but 2 bits in common get split into different clusters?
 
-def filter_nodes_by_distance_1(nodes_map, bits_number):
-    for node in nodes_map:
-        for i in range(0, bits_number):
-            neighbor = node ^ 1 << i
-            if neighbor in nodes_map:
-                yield (node, neighbor)
+def hamming_1(bits_length, distance):
+    for i in range(bits_length):
+        mask = 1 << i
+        yield distance ^ mask
 
-def filter_nodes_by_distance_2(nodes_map, bits_number):
-    for node in nodes_map:
-        for i in range(0, bits_number):
-            neighbor = node ^ 1 << i
-            for j in range(1, bits_number):
-                neighbor ^= 1 << j
-                if neighbor in nodes_map:
-                    yield (node, neighbor)
+def hamming_2(bits_length, distance):
+    for i in range(bits_length):
+        mask = 1 << i
+        for j in range(i + 1, bits_length):
+            mask ^= 1 << j
+            yield distance ^ mask
 
-def clustering(nodes_map, bits_number):
+def clustering(nodes_by_distance, bits_number):
     disjoint_set = DisjointSet()
     # initially put every node into separate cluster
-    for node in nodes_map.itervalues():
-        disjoint_set.make_set(node)
-    
-    for node, neighbor in filter_nodes_by_distance_1(nodes_map, bits_number):
-        pass
+    for nodes in nodes_by_distance.keys():
+        disjoint_set.make_set(nodes)
 
-    for node, neighbor in filter_nodes_by_distance_2(nodes_map, bits_number):
-        pass
+    for distance in nodes_by_distance.keys():
+        for neighbor in hamming_1(bits_number, distance):
+            if neighbor in nodes_by_distance:
+                node1_cluster = disjoint_set.get_node_by_id(distance)
+                node2_cluster = disjoint_set.get_node_by_id(neighbor)
+                disjoint_set.union(node1_cluster, node2_cluster)
 
+        for neighbor in hamming_2(bits_number, distance):
+            if neighbor in nodes_by_distance:
+                node1_cluster = disjoint_set.get_node_by_id(distance)
+                node2_cluster = disjoint_set.get_node_by_id(neighbor)
+                disjoint_set.union(node1_cluster, node2_cluster)
+
+    print('>>> last pass: ', disjoint_set.sets_counter)
 
 def get_data():
     with open("clustering_big.txt") as f:
-        nodes_map = {}
+        nodes_by_distance = defaultdict(list)
         lines = iter(f.readlines())
         bits_and_nodes_info_str = next(lines)
         nodes_number, bits_number = [int(v) for v in bits_and_nodes_info_str.split()]      
         for node, bit_distance in enumerate(lines, 1):
-            nodes_map[bit_distance] = node
-        return nodes_number, bits_number, nodes_map
-        
+            # remove tab and spaces
+            bit_distance = int(re.sub('\s+', '', bit_distance), 2)
+            nodes_by_distance[bit_distance].append(node)
+        return nodes_number, bits_number, nodes_by_distance
+
 if __name__ == '__main__':
-    get_data()
+    nodes_number, bits_number, nodes_by_distance = get_data()
+    clustering(nodes_by_distance, bits_number)
